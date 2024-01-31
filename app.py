51f758a621847222be7d00fa810962b986ab499c
BernardoliveiraFiap <RM551356@FIAP.COM.BR>
2024-01-31 12:14:46 -0300

    Construindofontes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
 
 sheet_alunos = workbook_alunos['Sheet1']
 
-for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)):
+for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2,max_row=2)):
     nome_curso = linha[0].value
     nome_participante = linha[1].value
     tipo_participante = linha[2].value
@@ -19,12 +19,16 @@ for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)):
     carga_horario = linha[5].value
     data_emissao = linha[6].value
 
-    fonte_nome = ImageFont.truetype('./tahomabd.ttf')
-    fonte_geral = ImageFont.truetype('./tahoma.ttf')
+    fonte_nome = ImageFont.truetype('./tahomabd.ttf',90)
+    fonte_geral = ImageFont.truetype('./tahoma.ttf',80)
+    fonte_data
 
     image = Image.open('./certificado_padrao.jpg')
     desenhar = ImageDraw.Draw(image)
 
-    desenhar.text((200, 600), nome_participante, fill='black', font=fonte_nome)
+    desenhar.text((1020, 827), nome_participante, fill='black', font=fonte_nome)
+    desenhar.text((1060, 950),nome_curso, fill='black',font=fonte_geral)
+    desenhar.text((1435,1065),tipo_participante, fill='black',font=fonte_geral)
+    desenhar.text((1480, 1182),str(carga_horario), fill='black',font=fonte_geral)
 
     image.save(f'./{indice}{nome_participante}certificado.png')
