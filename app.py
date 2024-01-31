458e3702c18aab5039209eb4c0360fdf61085e5b
BernardoliveiraFiap <RM551356@FIAP.COM.BR>
2024-01-31 12:02:09 -0300

    Gerando Certificados

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
 
 sheet_alunos = workbook_alunos['Sheet1']
 
-for linha in sheet_alunos.iter_rows(min_row=2):
+for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)):
     nome_curso = linha[0].value
     nome_participante = linha[1].value
     tipo_participante = linha[2].value
@@ -24,3 +24,7 @@ for linha in sheet_alunos.iter_rows(min_row=2):
 
     image = Image.open('./certificado_padrao.jpg')
     desenhar = ImageDraw.Draw(image)
+
+    desenhar.text((200, 600), nome_participante, fill='black', font=fonte_nome)
+
+    image.save(f'./{indice}{nome_participante}certificado.png')
